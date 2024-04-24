44e86e6d86d1ead0a10507cfb2bf34d90839d1c5
CalyxNZ <65590159+CalyxNZ@users.noreply.github.com>
2024-04-24 22:39:48 +1200

    migrate to mysql

---
+++ __init__.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_migrate import Migrate
+from .config import Config  # Add this import statement
+
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+migrate = Migrate()
+login_manager = LoginManager()
+login_manager.login_view = 'main.login'
+login_manager.login_message_category = 'info'
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'your_secret_key'
+    #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:pnXcwHvscCfVXkwhyINtmbhFARazQbCV@monorail.proxy.rlwy.net:55608/railway' #MySQL DB
+
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+
+    from app.routes import main
+
+    app.register_blueprint(main)
+
+    return app
