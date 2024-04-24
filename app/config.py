a161851b9a4b52665e24ed0e34c1ce30f8a5ba6d
CalyxNZ <65590159+CalyxNZ@users.noreply.github.com>
2024-04-24 23:54:59 +1200

    Update config.py

--- app/config.py
+++ app/config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:pnXcwHvscCfVXkwhyINtmbhFARazQbCV@monorail.proxy.rlwy.net:55608/railway'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:pnXcwHvscCfVXkwhyINtmbhFARazQbCV@monorail.proxy.rlwy.net:55608/railway'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
