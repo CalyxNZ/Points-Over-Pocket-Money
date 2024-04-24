15b86ef4c76db5144da546e2abadab24c635d6e8
CalyxNZ <65590159+CalyxNZ@users.noreply.github.com>
2024-04-24 23:40:00 +1200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -16,7 +16,7 @@ login_manager.login_message_category = 'info'
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'your_secret_key'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:pnXcwHvscCfVXkwhyINtmbhFARazQbCV@monorail.proxy.rlwy.net:55608/railway'
 
     db.init_app(app)
     migrate.init_app(app, db)
